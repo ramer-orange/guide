9ae94666361671c0a88c5560e021ef0b979c2de3
天野海 <kai@amanokainoMacBook-Air.local>
2024-12-03 09:14:04 +0900

    #39 編集ページ持ち物リストを追加

---
+++ app/Models/PackingItem.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PackingItem extends Model
+{
+    protected $fillable = [
+        'travel_id',
+        'packing_name',
+        'packing_is_checked'
+    ];
+
+    public function travelOverview()
+    {
+        return $this->belongsTo(TravelOverview::class, 'travel_id');
+    }
+}
