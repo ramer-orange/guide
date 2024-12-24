d5e301471b754c49f20c3bdb8efb2b0e848d3563
ramer-orange <ra.mer.web1111@gmail.com>
2024-12-24 11:33:48 +0900

    #100 バリデーションルール切り分け

---
+++ app/Http/Requests/SubmitFormRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use function Symfony\Component\Translation\t;
+
+class SubmitFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required | string | max:255',
+            'overviewText' => 'nullable | string',
+            'plans' => 'required | array',
+            'plans.*.date' => 'nullable | date',
+            'plans.*.time' => 'nullable | date_format:H:i',
+            'plans.*.plans_title' => 'nullable | string | max:255',
+            'plans.*.content' => 'nullable | string',
+            'plans.*.planFiles' => 'nullable|array',
+            'plans.*.planFiles.*' => 'nullable|file|mimes:jpg,jpeg,png,pdf,doc,docx|max:10240',
+            'packingItems' => 'required | array',
+            'packingItems.*.packing_name' => 'nullable | string | max:255',
+            'packingItems.*.packing_is_checked' => 'nullable | boolean',
+            'template_type' => 'nullable | string | max:255',
+            'souvenirs' => 'required | array',
+            'souvenirs.*.souvenirs_name' => 'nullable | string | max:255',
+            'souvenirs.*.souvenirs_is_checked' => 'nullable | boolean',
+            'additionalComments' => 'required | array',
+            'additionalComments.*.additionalComment_title' => 'nullable | string | max:255',
+            'additionalComments.*.additionalComment_text' => 'nullable | string',
+        ];
+    }
+}
--- app/Livewire/EditPlansForm.php
+++ app/Livewire/EditPlansForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Http\Requests\SubmitFormRequest;
 use App\Models\AdditionalComment;
 use App\Models\PackingItem;
 use App\Models\PlanFile;
@@ -33,6 +34,11 @@ class EditPlansForm extends Component
     public $deleteSouvenirs = [];
     public $additionalComments = [];
     public $deleteAdditionalComments = [];
+
+    protected function rules(): array
+    {
+        return (new SubmitFormRequest())->rules();
+    }
 
 
     /**
@@ -270,27 +276,7 @@ class EditPlansForm extends Component
 
     public function submit()
     {
-        $this->validate([
-            'title' => 'required | string | max:255',
-            'overviewText' => 'nullable | string',
-            'plans' => 'required | array',
-            'plans.*.date' => 'nullable | date',
-            'plans.*.time' => 'nullable | date_format:H:i',
-            'plans.*.plans_title' => 'nullable | string | max:255',
-            'plans.*.content' => 'nullable | string',
-            'plans.*.planFiles' => 'nullable | array',
-            'plans.*.planFiles.*' => 'nullable|file|mimes:jpg,jpeg,png,pdf,doc,docx|max:2048',
-            'packingItems' => 'required | array',
-            'packingItems.*.packing_name' => 'nullable | string | max:255',
-            'packingItems.*.packing_is_checked' => 'nullable | boolean',
-            'template_type' => 'nullable | string | max:255',
-            'souvenirs' => 'required | array',
-            'souvenirs.*.souvenir_name' => 'nullable | string | max:255',
-            'souvenirs.*.souvenir_is_checked' => 'nullable | boolean',
-            'additionalComments' => 'required | array',
-            'additionalComments.*.additionalComment_title' => 'nullable | string | max:255',
-            'additionalComments.*.additionalComment_text' => 'nullable | string',
-        ]);
+        $this->validate();
 
         $this->overview->update([
             'title' => $this->title,
